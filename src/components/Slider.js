ec9c8254e919bdf6244690b3e25ab1d5c4e3bb55
Bobby Caya <bob.caya1@gmail.com>
2018-03-20 20:38:26 -0600

    initial commit

---
+++ src/components/Slider.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+class Slider extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {} 
+  }
+  render() {
+    return (
+      <div className="slider">
+
+      </div>
+    );
+  }
+}
+
+export default Sliderl; 
